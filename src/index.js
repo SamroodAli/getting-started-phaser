dcb243bd13ca35917d89310e3577a184c0966cea
samroodAli <devsamrood@gmail.com>
2021-08-03 19:25:03 +0530

    Events:Add key press event animations and movements

--- src/index.js
+++ src/index.js
@@ -36,6 +36,8 @@ function preload() {
 
 var platforms;
 var player;
+var cursors;
+var colliding;
 
 function create() {
   this.add.image(400, 300, "sky");
@@ -52,7 +54,43 @@ function create() {
 
   player.setBounce(0.2);
   player.setCollideWorldBounds(true);
+
+  // sprite animations
+  this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+  this.anims.create({
+    key: "turn",
+    frames: [{ key: "dude", frame: 4 }],
+    frameRate: 20,
+  });
+
+  this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+  cursors = this.input.keyboard.createCursorKeys();
   this.physics.add.collider(platforms, player);
 }
 
-function update() {}
+function update() {
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+    player.anims.play("left", true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+
+    player.anims.play("right", true);
+  } else if (player.body.touching.down) {
+    player.setVelocity(0);
+    player.anims.play("turn");
+  }
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
+}
