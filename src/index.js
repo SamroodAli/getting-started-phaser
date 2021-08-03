905008e6de311a2d737feca051b8ffd06012d3cd
samroodAli <devsamrood@gmail.com>
2021-08-03 16:44:36 +0530

    Config:Import asset from the website and create a game instance

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import "./style.scss";
 import Phaser from "phaser";
-import Game from "./scenes/Game.js";
 
-export default new Phaser.Game({
+var config = {
   type: Phaser.AUTO,
-  width: 400,
-  height: 640,
-  scene: Game,
-});
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {}
+
+function create() {}
+
+function update() {}
