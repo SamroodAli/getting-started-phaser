18161dc5d974a262c47ed099a4c617a53c20cb00
samroodAli <devsamrood@gmail.com>
2021-08-03 18:15:13 +0530

    Platforms:Add platforms to the game scene

--- src/index.js
+++ src/index.js
@@ -10,6 +10,13 @@ var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
   scene: {
     preload: preload,
     create: create,
@@ -27,8 +34,20 @@ function preload() {
   this.load.spritesheet("dude", dude, { frameWidth: 32, frameHeight: 48 });
 }
 
+var platforms;
+
+var platforms;
+
 function create() {
   this.add.image(400, 300, "sky");
+
+  platforms = this.physics.add.staticGroup();
+
+  platforms.create(400, 568, "ground").setScale(2).refreshBody();
+
+  platforms.create(600, 400, "ground");
+  platforms.create(50, 250, "ground");
+  platforms.create(750, 220, "ground");
 }
 
 function update() {}
